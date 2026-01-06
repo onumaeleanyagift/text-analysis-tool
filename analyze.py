831c8a241d1e0e967f218c7033871f06545e9778
Eleanya Onuma <onumaeleanyagift@gmail.com>
2026-01-06 10:41:24 +0100

    Obtained username from terminal input

---
+++ analyze.py
@@ -0,0 +1,9 @@
+print("Welcome to the text analysis tool, I will mine and analyze a body of text from a file you give me")
+
+# Get username
+
+# print mmessage prompting user to input their name
+username = input("\nTo begin, please enter your username: ")
+
+# Greet the user
+print('Hello, ' + username)
