a0129421ff9549689c858bc0b8b65646b8e541fd
Eleanya Onuma <onumaeleanyagift@gmail.com>
2026-04-23 23:32:19 +0100

    Added analyze stock request handle

--- server.py
+++ server.py
@@ -2,9 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Si Scholarship 2027'
+@app.route('/si-se')
+def si_se():
+    return 'SI Scholarship 2027'
+
+@app.route('/analyze-stock')
+def analyzeStock():
+    return {'data': 'Coming soon'}
+
 
 if __name__ == '__main__':
     app.run()
